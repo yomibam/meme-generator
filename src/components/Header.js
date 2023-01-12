53992a64b17471ed5e062555b4b74e33ed781542
yomibam <yomibamidele23@gmail.com>
2023-01-12 14:24:08 +0100

    changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <header className="header">
       <img src="../images/troll-face.png" className="header--image" />
-      <h2 className="header--title">Meme Generator</h2>
+      <h2 className="header--title">Mem Generator</h2>
       <h4 className="header--project">React Course - Project 3</h4>
     </header>
   );
