1afc4f5d07ae141e6b9efc0db6a7d0f8fe22c346
yomibam <yomibamidele23@gmail.com>
2023-01-12 14:27:28 +0100

    changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,8 @@ import React from "react";
 export default function Header() {
   return (
     <header className="header">
-      <img src="../images/troll-face.png" className="header--image" />
-      <h2 className="header--title">Mem Generator</h2>
+      <img src="../images/troll-face.png" alt="" className="header--image" />
+      <h2 className="header--title">Meme Generator</h2>
       <h4 className="header--project">React Course - Project 3</h4>
     </header>
   );
